d95f08d02ae0001d174ff4953519c19cd77b0d8f
petrenko-am <petrenko.am@mail.ru>
2016-02-21 11:40:49 +0700

    eserv

--- asserv.py
+++ asserv.py
@@ -1,9 +1,10 @@
 import asyncore
+import socket
 
 class EchoHandler(asyncore.dispatcher_with_send):
 
     def handle_read(self):
-        data = self.recv(8192)
+        data = self.recv(1024)
         if data:
             self.send(data)
 
@@ -11,14 +12,17 @@ class EchoServer(asyncore.dispatcher):
 
     def __init__(self, host, port):
         asyncore.dispatcher.__init__(self)
-        self.create_socket()
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.set_reuse_addr()
         self.bind((host, port))
-        self.listen(5)
+        self.listen(10)
 
-    def handle_accepted(self, sock, addr):
-        print('Incoming connection from %s' % repr(addr))
-        handler = EchoHandler(sock)
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is not None:
+            sock, addr = pair
+            print 'Incoming connection from %s' % repr(addr)
+            handler = EchoHandler(sock)
 
 server = EchoServer('', 2222)
 asyncore.loop()
