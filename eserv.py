a6a63b85b07c67908a7fcfa507829be9ff99e110
petrenko-am <petrenko.am@mail.ru>
2016-02-21 09:55:55 +0700

    eserv

--- eserv.py
+++ eserv.py
@@ -1,43 +1,16 @@
-import asyncio
-import logging
-import concurrent.futures
-
-class EchoServer(object):
-	"""Echo server class"""
-	
-	def __init__(self, host, port, loop=None):
-		self._loop = loop or asyncio.get_event_loop() 
-		self._server = asyncio.start_server(self.handle_connection, host=host, port=port)
-	
-	def start(self, and_loop=True):
-		self._server = self._loop.run_until_complete(self._server)
-		logging.info('Listening established on {0}'.format(self._server.sockets[0].getsockname()))
-		if and_loop:
-			self._loop.run_forever()
-	
-	def stop(self, and_loop=True):
-		self._server.close()
-		if and_loop:
-			self._loop.close()
-	
-	@asyncio.coroutine    
-	def handle_connection(self, reader, writer):
-		peername = writer.get_extra_info('peername')
-		logging.info('Accepted connection from {}'.format(peername))
-		while not reader.at_eof():
-			try:
-				data = yield from asyncio.wait_for(reader.readline(), timeout=10.0)
-				writer.write(data)
-			except concurrent.futures.TimeoutError:
-				break
-		writer.close()
-
-if __name__ == '__main__':
-	logging.basicConfig(level=logging.DEBUG)
-	server = EchoServer('127.0.0.1', 2007)
-	try:
-		server.start()
-	except KeyboardInterrupt:
-		pass # Press Ctrl+C to stop
-	finally:
-		server.stop()
+import socket
+																				
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('10.42.234.66', 2222))
+s.listen(5)
+client, address = s.accept()
+while True:
+	print(address)
+	data = s.recv(1024)
+	clent.setimeout(60)
+	client.close() 
+	if bytes('close'.encode('utf-8')) in data:
+		s.close()
+		break;
+	if data:
+		clinet.send(data)
